057b9206d7d0229fb6ae25b273f02dacd8837b15
Lani <lanisuin@gmail.com>
2022-01-15 15:25:53 -0500

    Instant Placement (no tap)

--- app/src/main/java/com/example/clewapplication/SingleUseRouteActivity.java
+++ app/src/main/java/com/example/clewapplication/SingleUseRouteActivity.java
@@ -8,11 +8,16 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.ar.core.Anchor;
+import com.google.ar.core.Frame;
 import com.google.ar.core.HitResult;
 import com.google.ar.core.Plane;
+import com.google.ar.core.Pose;
 import com.google.ar.core.Session;
 //Change the Gradle file for the below imports
+import com.google.ar.core.TrackingState;
 import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.FrameTime;
+import com.google.ar.sceneform.Node;
 import com.google.ar.sceneform.rendering.ModelRenderable;
 import com.google.ar.sceneform.ux.ArFragment;
 import com.google.ar.sceneform.ux.BaseArFragment;
@@ -26,6 +31,7 @@ public class SingleUseRouteActivity extends AppCompatActivity {
 
     private Session session;
     private ModelRenderable modelRenderable;
+    private boolean placed = false;
 
     //Add ARMode, Look through InstantPlacementPoint.TrackingMethod
 
@@ -35,6 +41,7 @@ public class SingleUseRouteActivity extends AppCompatActivity {
         setContentView(R.layout.activity_single_use_route);
 
         arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.fragment);
+        arFragment.getArSceneView().getScene().addOnUpdateListener(this::onUpdateFrame);
         setupModel();
         setUpPlane();
     }
@@ -74,5 +81,30 @@ public class SingleUseRouteActivity extends AppCompatActivity {
         node.setParent(anchorNode);
         node.setRenderable(modelRenderable);
         node.select();
+    }
+
+    private void onUpdateFrame(FrameTime frameTime) {
+
+        Frame frame = arFragment.getArSceneView().getArFrame();
+
+        // If there is no frame, just return.
+        if (frame == null) {
+            return;
+        }
+
+        //Making sure ARCore is tracking some feature points, makes the augmentation little stable.
+        if(frame.getCamera().getTrackingState()== TrackingState.TRACKING && !placed) {
+
+            Pose pos = frame.getCamera().getPose().compose(Pose.makeTranslation(0, 0, -0.3f));
+            Anchor anchor = arFragment.getArSceneView().getSession().createAnchor(pos);
+            AnchorNode anchorNode = new AnchorNode(anchor);
+            anchorNode.setParent(arFragment.getArSceneView().getScene());
+
+            // Create the arrow node and add it to the anchor.
+            Node arrow = new Node();
+            arrow.setParent(anchorNode);
+            arrow.setRenderable(modelRenderable);
+            placed = true; //to place the arrow just once.
+        }
     }
 }

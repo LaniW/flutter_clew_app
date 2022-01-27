8e293c65adce63dc3df5f1dd0dd81d3eca7d6dac
Lani <lanisuin@gmail.com>
2022-01-27 14:03:42 -0500

    Catch up activity save route

--- app/src/main/java/com/example/clewapplication/SaveRouteActivity.java
+++ app/src/main/java/com/example/clewapplication/SaveRouteActivity.java
@@ -23,6 +23,8 @@ import com.google.ar.sceneform.ux.ArFragment;
 import com.google.ar.sceneform.ux.BaseArFragment;
 import com.google.ar.sceneform.ux.TransformableNode;
 
+import java.util.ArrayList;
+
 public class SaveRouteActivity extends AppCompatActivity {
 
     private static final String TAG = SaveRouteActivity.class.getSimpleName();
@@ -31,7 +33,12 @@ public class SaveRouteActivity extends AppCompatActivity {
 
     private Session session;
     private ModelRenderable modelRenderable;
-    private boolean placed = false;
+    private boolean b = true;
+    private Node newCrumb = new Node();
+    private Node fEndpoint = new Node();
+    private Node LEndpoint = new Node();
+    private Node waypoint = new Node();
+    private ArrayList<Node> coordinatesList = new ArrayList<Node>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -39,6 +46,7 @@ public class SaveRouteActivity extends AppCompatActivity {
         setContentView(R.layout.activity_save_route);
 
         arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.fragment);
+        arFragment.getArSceneView().getScene().addOnUpdateListener(this::onUpdateFrame);
         setupModel();
         setUpPlane();
     }
@@ -84,24 +92,44 @@ public class SaveRouteActivity extends AppCompatActivity {
 
         Frame frame = arFragment.getArSceneView().getArFrame();
 
-        // If there is no frame, just return.
         if (frame == null) {
             return;
         }
 
-        //Making sure ARCore is tracking some feature points, makes the augmentation little stable.
-        if(frame.getCamera().getTrackingState()== TrackingState.TRACKING && !placed) {
+        if(frame.getCamera().getTrackingState()== TrackingState.TRACKING) {
 
-            Pose pos = frame.getCamera().getPose().compose(Pose.makeTranslation(0, 0, -0.3f));
+            Pose pos = frame.getCamera().getPose().compose(Pose.makeTranslation(0, 0, 0));
             Anchor anchor = arFragment.getArSceneView().getSession().createAnchor(pos);
             AnchorNode anchorNode = new AnchorNode(anchor);
             anchorNode.setParent(arFragment.getArSceneView().getScene());
 
-            // Create the arrow node and add it to the anchor.
-            Node arrow = new Node();
-            arrow.setParent(anchorNode);
-            arrow.setRenderable(modelRenderable);
-            placed = true; //to place the arrow just once.
+            Node crumb = new Node();
+            crumb.setParent(anchorNode);
+
+            double distanceValue = Math.sqrt((crumb.getWorldPosition().x - newCrumb.getWorldPosition().x) * (crumb.getWorldPosition().x - newCrumb.getWorldPosition().x) + (crumb.getWorldPosition().y - newCrumb.getWorldPosition().y) * (crumb.getWorldPosition().y - newCrumb.getWorldPosition().y) + (crumb.getWorldPosition().z - newCrumb.getWorldPosition().z) * (crumb.getWorldPosition().z - newCrumb.getWorldPosition().z));
+            //half a meter~ (in the x, y and z direction)
+            //render path
+
+            if (b || distanceValue >= 0.5) {
+                /*
+                if(b || last crumb || (x >= 0.5) || (y >= 0.5) || (z >= 0.5)){
+                    //render as sphere
+                }else{
+                    //render as cone
+                }
+                */
+                crumb.setRenderable(modelRenderable);
+                newCrumb = crumb;
+
+                coordinatesList.add(crumb);
+                for (Node n: coordinatesList)
+                {
+                    System.out.println("COORDINATE:" + n.getWorldPosition()); //TESTING
+                    fEndpoint = coordinatesList.get(0);
+                    LEndpoint = coordinatesList.get(coordinatesList.size() - 1);
+                }
+                b = false;
+            }
         }
     }
 }
